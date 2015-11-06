f9f51da372fc70a82b61b453bd4bca069a490e3f
maikeld86 <m.doemges86@ziggo.nl>
2015-11-06 15:40:06 +0100

    controllers

---
+++ resources/views/pages/about.blade.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html lang="en" xmlns="http://www.w3.org/1999/html">
+    <head>
+        <meta charset="UTF-8">
+        <title>About</title>
+    </head>
+    <body>
+        <h1>About Me: {{ $name }} </h1>
+        <p>
+            Ik ben Maikel,</br>
+            En ik ben Laravel 5 aan het leren
+        </p>
+    </body>
+</html>
