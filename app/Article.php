7d77dbdba16e18fc3d17d99260e3fee2d11d3e95
maikeld86 <m.doemges86@ziggo.nl>
2015-11-13 13:59:54 +0100

    Many to Many relations (with tags)

--- app/Article.php
+++ app/Article.php
@@ -40,4 +40,13 @@ class Article extends Model
     {
         return $this->belongsTo('App\User');
     }
+    /*
+     * Get the tags associated with the given article
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\belongsToMany
+     */
+    public function tags()
+    {
+        return $this->belongsToMany('App\Tag')->withTimestamps();
+    }
 }
