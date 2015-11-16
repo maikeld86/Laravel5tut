d91bbc140dc9c151dbdf2388d1ee2ff7bd55bb1d
maikeld86 <m.doemges86@ziggo.nl>
2015-11-16 15:56:41 +0100

    update Syncing Tags

--- app/Http/Controllers/ArticlesController.php
+++ app/Http/Controllers/ArticlesController.php
@@ -13,7 +13,10 @@ use App\Tag;
 
 class ArticlesController extends Controller
 {
-
+    /*
+     * create a new article controller instance
+     *
+     * */
     public function __construct()
     {
         $this->middleware('auth',['except' => ['index','show']]);
@@ -39,12 +42,6 @@ class ArticlesController extends Controller
      * */
     public function show(Article $article)
     {
-        //oude methode
-        //$article = Article::findOrFail($id);
-
-        // testing
-        //dd($article->created_at->addDays(8)->diffforHumans());
-        //dd($article->published_at);
 
         return view('articles.show',compact('article'));
     }
@@ -58,21 +55,19 @@ class ArticlesController extends Controller
     public function create()
     {
         $tags = Tag::lists('name','id');
+
         return view('articles.create',compact('tags'));
     }
 
     /*
-     * Save an article
+     *  Save an article
      *
-     *
-     *@return response
+     *  @param  ArticleRequest $request
+     *  @return response
      * */
     public function store(ArticleRequest $request)
     {
-
-        $article = Auth::user()->articles()->create($request->all());
-
-        $article->tags()->attach($request->input('tag_list'));
+        $this->createArticle($request);
 
         flash('You are now Logged in');
 
@@ -88,24 +83,49 @@ class ArticlesController extends Controller
     public function edit(Article $article)
     {
         $tags = Tag::lists('name','id'); // tijdelijk
-        //$article = Article::findOrFail($id); oude methode
 
         return view('articles.edit',compact('article','tags'));
     }
     /*
     * update an article
     *
+    *@param ArticleRequest $request
     *@param Article $article
     *@return response
     * */
-    public function update(Article $article, ArticleRequest $request)
+    public function update(ArticleRequest $request, Article $article)
     {
-        //$article = Article::findOrFail($id); oude methode
-
         $article->update($request->all());
+
+        $this->syncTags($article, $request->input('tag_list'));
 
         return redirect('articles');
     }
+
+    /**
+     * sync up the lists of tags in the database
+     * @param Article $article
+     * @param array   $tags
+     */
+    private function syncTags(Article $article, array $tags)
+    {
+        $article->tags()->sync($tags);
+    }
+
+    /*
+     * save a new article
+     * $param   ArticleRequest $request
+     * @return  mixed
+     * */
+    private function createArticle(ArticleRequest $request)
+    {
+        $article = Auth::user()->articles()->create($request->all());
+
+        $this->syncTags($article, $request->input('tag_list'));
+
+        return $article;
+    }
+
 }
 
 
